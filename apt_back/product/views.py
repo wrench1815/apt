42ef437ca9646731eee87163795ea832d703a87b
wrench1815 <hardeepkumar1815@gmail.com>
2022-11-04 17:08:15 +0530

    👌 IMPROVE: update view logic for retrieve and update

--- apt_back/product/views.py
+++ apt_back/product/views.py
@@ -1,16 +1,51 @@
 from rest_framework import generics, status
-from rest_framework import response
-from rest_framework.filters import SearchFilter
+from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductUpdateSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['$name']
+    ordering_fields = ['date_added']
+    ordering = '-date_added'
 
 
-class ProductRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+class ProductRetrieveUpdateAPIView(generics.GenericAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductUpdateSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        product = self.get_object()
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    def patch(self, request, *args, **kwargs):
+        product = self.get_object()
+
+        try:
+            serializer = ProductUpdateSerializer(
+                product,
+                data=request.data,
+                partial=True,
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+        except Exception as ex:
+            return Response(
+                {'detail': str(ex)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        response = ProductSerializer(product)
+
+        return Response(
+            response.data,
+            status=status.HTTP_200_OK,
+        )
